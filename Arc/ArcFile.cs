08e660af3d662628cda05703cf162fb41506b7d4
Evan Lawrence <mailing.terminal.cs@gmail.com>
2021-11-09 21:18:55 -0800

    upload remaining code

--- Arc/ArcFile.cs
+++ Arc/ArcFile.cs
@@ -3,35 +3,35 @@ using static System.IO.File;
 
 namespace Arc
 {
-    /// <summary>
-    /// Arcfile, made by terminal.cs. (Version 1.0)
-    /// </summary>
+    /// <summary> Arcfile, made by terminal.cs. (Version 1.0) </summary>
     public class ArcFile
     {
         private readonly string Path = "";
         private readonly Dictionary<string, string> Arcs = new Dictionary<string, string>();
-        private readonly double ArcVersion = 1.0;
-
         public ArcFile(string ArcPath)
         {
             Path = ArcPath;
-            foreach (string str in ReadAllLines(ArcPath))
+            if (Exists(Path))
             {
-                Arcs.Add(str.Split('=')[0], str.Split('=')[1]);
+                foreach (string str in ReadAllLines(Path))
+                {
+                    Arcs.Add(str.Split('=')[0], str.Split('=')[1]);
+                }
             }
+            else { WriteAllText(Path, ""); }
         }
 
-        public virtual string Read(string Key) => Arcs[Key];
-        public virtual void Create(string Key, string Value) => Arcs.Add(Key, Value);
-        public virtual void Modify(string Key, string Value) => Arcs[Key] = Value;
-        public virtual bool Exists(string Key) => Arcs.ContainsKey(Key);
-        public virtual void Remove(string Key) => Arcs.Remove(Key);
-        public virtual void Save()
+        public string Read(string Key) => Arcs[Key];
+        public void Create(string Key, string Value) => Arcs.Add(Key, Value);
+        public void Modify(string Key, string Value) => Arcs[Key] = Value;
+        public bool Exists(string Key) => Arcs.ContainsKey(Key);
+        public void Remove(string Key) => Arcs.Remove(Key);
+        public void Save()
         {
-            string Final = "ArcVersion=" + ArcVersion;
-            foreach (var Pair in Arcs)
+            string Final = "";
+            foreach (KeyValuePair<string, string> Pair in Arcs)
             {
-                Final = Final + "\n" + Pair.Key + "=" + Pair.Value;
+                Final = Final + Pair.Key + "=" + Pair.Value + "\n";
             }
             WriteAllText(Path, Final);
         }
